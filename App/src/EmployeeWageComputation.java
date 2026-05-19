a9f0689bc15b683901b8afcb24d269a2d982812b
sandeep <sandeepmishra898k@gmail.com>
2026-05-19 17:25:11 +0530

    UC2 – Calculate Daily Employee Wage

--- App/src/EmployeeWageComputation.java
+++ App/src/EmployeeWageComputation.java
@@ -3,12 +3,16 @@ public class EmployeeWageComputation {
     public static void main(String[] args) {
 
         int IS_PRESENT = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int EMP_HOURS = 8;
 
         double empCheck = Math.floor(Math.random() * 10) % 2;
 
-        if (empCheck == IS_PRESENT)
-            System.out.println("Employee is Present");
-        else
+        if (empCheck == IS_PRESENT) {
+            int salary = EMP_HOURS * EMP_RATE_PER_HOUR;
+            System.out.println("Employee Wage : " + salary);
+        } else {
             System.out.println("Employee is Absent");
+        }
     }
 }
